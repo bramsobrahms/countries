8ef97687479850c83f05f9885b1635155ad190bd
Brahms <bramso.brahms@gmail.com>
2024-12-23 18:25:53 +0100

    Header with rotationg earth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './core/header/header.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    RouterOutlet
+  ],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
