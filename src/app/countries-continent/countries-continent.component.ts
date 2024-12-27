a7c3ec90c4062c32bd95bd12395af70580e2d10b
Brahms <bramso.brahms@gmail.com>
2024-12-27 19:26:18 +0100

    create html card for countries with some information about

--- src/app/countries-continent/countries-continent.component.ts
+++ src/app/countries-continent/countries-continent.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {ContinentModel} from '../models/continent.model';
 import {CountriesService} from '../services/countries.service';
 import {ActivatedRoute} from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-countries-continent',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './countries-continent.component.html',
   styleUrl: './countries-continent.component.scss'
 })
